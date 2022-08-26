30f0d9c3d20e7a789e94db209811e1f9720711d9
Charles Miller <charles.miller.0412@gmail.com>
2022-08-26 14:10:34 -0500

    added h1 for validation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Modal from "./Components/Modal";
 function App() {
     return (
         <main className="App">
+            <h1 style={{ display: "none" }}>
+                Charles Miller - advice generator
+            </h1>
             <Modal />
         </main>
     );
